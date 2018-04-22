363cb07fa32bab5752ecf987a0365cdab822e626
dovelol <312085082@qq.com>
2018-04-22 13:38:48 +0800

    代码提交

---
+++ src/main/java/com/dove/lol/dovelol/demo/date201804/demo20180422/WriteFileTest.java
@@ -0,0 +1,74 @@
+package com.dove.lol.dovelol.demo.date201804.demo20180422;
+
+import com.dove.lol.dovelol.utils.GlobalTheadPool;
+
+import java.io.*;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Dovelol
+ * @date 2018/4/22 11:36
+ */
+public class WriteFileTest {
+
+    public static void main(String[] args) {
+        new Thread(() -> {
+            File file = new File("D:\\text.txt");
+            try (FileOutputStream fos = new FileOutputStream(file)) {
+                TimeUnit.MILLISECONDS.sleep(1);
+                for (int i = 0 ; i < 100; i++){
+                    String str = "1 wirte file test!";
+                    fos.write(str.getBytes());
+                }
+
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        new Thread(() -> {
+            File file = new File("D:\\text.txt");
+            try (FileOutputStream fos = new FileOutputStream(file)) {
+                TimeUnit.MILLISECONDS.sleep(1);
+                for (int i = 0 ; i < 100; i++){
+                    String str = "2 wirte file test!";
+                    fos.write(str.getBytes());
+                }
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        new Thread(() -> {
+            File file = new File("D:\\text.txt");
+            try (FileOutputStream fos = new FileOutputStream(file)) {
+                TimeUnit.MILLISECONDS.sleep(1);
+                for (int i = 0 ; i < 100; i++){
+                    String str = "3 wirte file test!";
+                    fos.write(str.getBytes());
+                }
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+
+    }
+
+
+}
