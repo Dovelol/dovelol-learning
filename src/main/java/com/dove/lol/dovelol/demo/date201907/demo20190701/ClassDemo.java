782df75e9bc552b871a818874193ae5b346bc171
dovelol <312085082@qq.com>
2019-07-01 23:44:33 +0800

    java demo

---
+++ src/main/java/com/dove/lol/dovelol/demo/date201907/demo20190701/ClassDemo.java
@@ -0,0 +1,14 @@
+package com.dove.lol.dovelol.demo.date201907.demo20190701;
+
+/**
+ * @author Dovelol
+ * @date 2019/7/1 23:43
+ */
+public class ClassDemo {
+
+
+    public static void main(String[] args) {
+        System.out.println(System.getenv());
+    }
+
+}
