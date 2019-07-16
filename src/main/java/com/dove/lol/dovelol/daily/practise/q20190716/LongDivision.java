38f8ca8dd3b59ea395c78a0d43ffc7e07c7d8279
dovelol <312085082@qq.com>
2019-07-16 21:43:03 +0800

    java demo

---
+++ src/main/java/com/dove/lol/dovelol/daily/practise/q20190716/LongDivision.java
@@ -0,0 +1,29 @@
+package com.dove.lol.dovelol.daily.practise.q20190716;
+
+/**
+ * @author Dovelol
+ * @date 2019/7/16 21:23
+ */
+public class LongDivision {
+
+    /**
+     * 以上程序输出内容是？
+
+     *  (a) 5
+     *  (b) 1000
+     *  (c) 抛出异常
+     *  (d) 以上答案都不对
+     */
+
+
+    private static final long MILLIS_PER_DAY
+            = 24 * 60 * 60 * 1000;
+
+    private static final long MICROS_PER_DAY
+            = 24 * 60 * 60 * 1000 * 1000;
+
+    public static void main(String[] args) {
+        System.out.println(MICROS_PER_DAY / MILLIS_PER_DAY);
+    }
+
+}
