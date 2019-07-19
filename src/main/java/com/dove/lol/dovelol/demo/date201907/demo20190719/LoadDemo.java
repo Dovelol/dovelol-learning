24249bdd93b6ceba5fa1c6657c1991f7bd91fe06
dovelol <312085082@qq.com>
2019-07-19 22:32:09 +0800

    java demo

---
+++ src/main/java/com/dove/lol/dovelol/demo/date201907/demo20190719/LoadDemo.java
@@ -0,0 +1,23 @@
+package com.dove.lol.dovelol.demo.date201907.demo20190719;
+
+/**
+ * @author Dovelol
+ * @date 2019/7/19 22:22
+ */
+public class LoadDemo {
+
+    private static String d = "demo";
+
+    public LoadDemo() {
+        System.out.println("LoadDemo");
+    }
+
+    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+        //System.out.println(d);
+        Class<?> aClass = Class.forName("com.dove.lol.dovelol.demo.date201907.demo20190719.LoadDemo");
+        System.out.println(aClass);
+        Object o = aClass.newInstance();
+        System.out.println(o);
+    }
+
+}
