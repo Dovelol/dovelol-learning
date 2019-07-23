c7550dfd8cdd3ca1f3fcf0c61627a9fb34b06f36
dovelol <312085082@qq.com>
2019-07-23 23:34:58 +0800

    java demo

---
+++ src/main/java/com/dove/lol/dovelol/daily/practise/q20190723/PowerOfTen.java
@@ -0,0 +1,40 @@
+package com.dove.lol.dovelol.daily.practise.q20190723;
+
+/**
+ * @author Dovelol
+ * @date 2019/7/23 23:32
+ */
+public enum PowerOfTen {
+
+    /**
+     *  以上程序输出内容是？
+
+     *  (a) one ten hundred
+     *  (b) one ten 100
+     *  (c) 编译错误
+     *  (d) 以上都不是
+     */
+
+
+    ONE(1), TEN(10),
+    HUNDRED(100) {
+        @Override
+        public String toString() {
+            return Integer.toString(HUNDRED.val);
+        }
+    };
+    private final int val;
+
+    PowerOfTen(int val) {
+        this.val = val;
+    }
+
+    @Override
+    public String toString() {
+        return name().toLowerCase();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(ONE + " " + TEN + " " + HUNDRED);
+    }
+}
