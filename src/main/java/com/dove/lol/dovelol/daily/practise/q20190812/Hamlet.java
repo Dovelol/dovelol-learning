87b7de58b87c36f71806dc4493e547344fa5150d
dovelol <312085082@qq.com>
2019-08-12 23:21:26 +0800

    java demo

---
+++ src/main/java/com/dove/lol/dovelol/daily/practise/q20190812/Hamlet.java
@@ -0,0 +1,31 @@
+package com.dove.lol.dovelol.daily.practise.q20190812;
+
+import java.util.Random;
+
+/**
+ * @author Dovelol
+ * @date 2019/8/12 23:12
+ */
+public class Hamlet {
+
+    /**
+     *  以上程序输出内容是？
+     *  (a)运行时异常
+     *  (b)3
+     *  (c)1.0
+     *  (d)以上答案都不是
+     *
+     *  D
+     */
+
+    public static void main(String[] args) {
+
+        Random rnd  = new Random();
+
+        boolean toBe = rnd.nextBoolean();
+
+        Number result = (toBe || !toBe)?new Integer(3):new Float(1);
+
+        System.out.println(result);
+    }
+}
