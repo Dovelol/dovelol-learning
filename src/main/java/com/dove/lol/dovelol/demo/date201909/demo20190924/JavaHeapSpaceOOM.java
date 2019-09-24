bb761e834c38083c7a3184ae0669c54c0db2c64e
dovelol <312085082@qq.com>
2019-09-24 22:01:30 +0800

    java demo

---
+++ src/main/java/com/dove/lol/dovelol/demo/date201909/demo20190924/JavaHeapSpaceOOM.java
@@ -0,0 +1,16 @@
+package com.dove.lol.dovelol.demo.date201909.demo20190924;
+
+/**
+ * @author Dovelol
+ * @date 2019/9/24 21:55
+ */
+public class JavaHeapSpaceOOM {
+
+    public static void main(String[] args) {
+        // 2M
+        int size = 2*1024*1024;
+
+        int[] array = new int[size];
+    }
+
+}
