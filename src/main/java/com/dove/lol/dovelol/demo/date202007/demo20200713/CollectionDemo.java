1f560fd4b97e836a93b1ae90ed34ae1cf2981929
yuanzheng <yuanzheng@zhongan.com>
2020-07-14 22:56:58 +0800

    java demo

---
+++ src/main/java/com/dove/lol/dovelol/demo/date202007/demo20200713/CollectionDemo.java
@@ -0,0 +1,22 @@
+package com.dove.lol.dovelol.demo.date202007.demo20200713;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * @author Dovelol
+ * @date 2020/7/13 22:01
+ */
+public class CollectionDemo {
+
+    public static void main(String[] args) {
+        TreeSet<String> str1 = new TreeSet<>();
+
+        str1.add("13");
+        str1.add("3334");
+        str1.add("13213");
+        str1.add("11132313");
+        str1.add("65467543");
+    }
+
+}
